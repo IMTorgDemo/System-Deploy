00fb6010b5bc0cbebfd993f85ba96ea580588334
Jason <jason.beach@mgmt-tech.org>
2017-12-10 23:18:39 -0500

    add production env

---
+++ WebApp/config/env/production.js
@@ -0,0 +1,33 @@
+module.exports = {
+    connection: {
+        protocol: 'http',
+        host: process.env.HOST || '127.0.0.1',
+        port: process.env.PORT || 3000
+    },
+    database: {
+        domain: 'mongodb://',
+        ipaddress: 'data_store:',
+        port: '27017/',
+        collection: 'foo'
+    },
+    logs: {
+        location: process.cwd() + "/log"
+    },
+    cors: {
+        origin: ['*'],
+        headers: [
+            "Accept",
+            "Authorization",
+            "Content-Type",
+            "content-type",
+            "If-None-Match",
+            "Accept-language",
+            "access_token",
+            "Cache-Control",
+            "Pragma",
+            "WWW-Authenticate",
+            "Server-Authorization",
+            "X-HTTP-Method-Override"
+        ]
+    }
+}
